83e5283db6ef353d4bcf7f0ac05714f1aa773ea5
Taha Tuna <tahatuna777@gmail.com>
2023-04-21 00:13:51 +0200

    Initial Commit

---
+++ CCMB/CCMBApp.swift
@@ -0,0 +1,17 @@
+//
+//  CCMBApp.swift
+//  CCMB
+//
+//  Created by Taha Tuna on 21.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct CCMBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
