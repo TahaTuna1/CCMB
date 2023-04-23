fdf7ec39a3c774d3e8f2f1126c32f9c595b77620
Taha Tuna <tahatuna777@gmail.com>
2023-04-23 22:39:31 +0200

    ListView for All - Solution not Elegant

--- CCMB/CurrencyBar/CurrencyBarView.swift
+++ CCMB/CurrencyBar/CurrencyBarView.swift
@@ -14,6 +14,14 @@ struct CurrencyBarView: View {
     @State private var selectedItem = ""
     @State var isShowingList: Bool = false
     
+    @State var isShowingList2: Bool = false// Not the best solution, is it?
+    @State var isShowingList3: Bool = false
+    @State var isShowingList4: Bool = false
+    
+    @State private var selectedItem2 = "" // I hate it
+    @State private var selectedItem3 = ""
+    @State private var selectedItem4 = ""
+    
     var body: some View {
         
         ZStack {
@@ -35,17 +43,9 @@ struct CurrencyBarView: View {
                         isShowingList = true
                     }//MARK: Popover List
                     .popover(isPresented: $isShowingList, arrowEdge: .leading) {
-                        List(selection: $selectedItem) {
-                            ForEach(0 ..< viewModel.allCurrencies.count, id: \.self) { index in
-                                HStack {
-                                    Text(viewModel.allCurrencies[index].name)
-                                    Spacer()
-                                    Text(viewModel.allCurrencies[index].symbol_native)
-                                    
-                                }.font(.body).tag(viewModel.allCurrencies[index].code)
-                            }
-                        }
-                        .frame(height: 150)
+                        
+                        ListView(viewModel: viewModel, selectedItem: $selectedItem)
+                        
                     }// ON CHANGE
                     .onChange(of: selectedItem) { value in
                         viewModel.baseCurrency = value
@@ -77,25 +77,35 @@ struct CurrencyBarView: View {
                     
                 }
                 .frame(height: 50)
-                .background(Color("Custom")) //MARK: Change This Custom Color
-                
                 
                 
                 // 2nd 3rd 4th Currencies
                 SecondaryCurrencyView(
-                    name: viewModel.secondCurrency.name,
-                    amount: enteredAmount * viewModel.secondCurrency.amount)
+                    name: $viewModel.secondCurrency.name,
+                    amount: enteredAmount * viewModel.secondCurrency.amount,
+                isShowingList: $isShowingList2,
+                viewModel: viewModel,
+                selectedItem: $selectedItem2)
                 .blur(radius: viewModel.isLoading ? 2 : 0)
                 
                 SecondaryCurrencyView(
-                    name: viewModel.thirdCurrency.name,
-                    amount: enteredAmount * viewModel.thirdCurrency.amount)
+                    name: $viewModel.thirdCurrency.name,
+                    amount: enteredAmount * viewModel.thirdCurrency.amount,
+                    isShowingList: $isShowingList3,
+                    viewModel: viewModel,
+                    selectedItem: $selectedItem3)
                 .blur(radius: viewModel.isLoading ? 2 : 0)
                 
                 SecondaryCurrencyView(
-                    name: viewModel.fourthCurrency.name,
-                    amount: enteredAmount * viewModel.fourthCurrency.amount)
+                    name: $viewModel.fourthCurrency.name,
+                    amount: enteredAmount * viewModel.fourthCurrency.amount,
+                    isShowingList: $isShowingList4,
+                    viewModel: viewModel,
+                    selectedItem: $selectedItem4)
                 .blur(radius: viewModel.isLoading ? 2 : 0)
+                
+                
+                
                 
                 
                 
@@ -139,14 +149,11 @@ struct CurrencyBarView: View {
                         Image(systemName: "power.circle")
                             .frame(width: 40)
                             .font(.title2)
-                            .foregroundColor(.red)
+                            .foregroundColor(.white)
                     }
                 }
                 .frame(height: 50)
-                .background(.white.opacity(0.3))
                 .buttonStyle(.borderless)
-                
-                
                 
                 
                 
@@ -163,8 +170,12 @@ struct CurrencyBarView: View {
 
 // Small Currencies - 2nd, 3rd, 4th
 struct SecondaryCurrencyView: View{
-    var name: String
+    @Binding var name: String
     var amount: Double
+    @Binding var isShowingList: Bool
+    @ObservedObject var viewModel: CurrencyViewModel
+    @Binding var selectedItem: String
+    
     
     var body: some View{
         HStack{
@@ -173,17 +184,57 @@ struct SecondaryCurrencyView: View{
                     .padding(.leading, 5)
                 Image(systemName: "chevron.down").font(.caption)
             }
+            .popover(isPresented: $isShowingList, arrowEdge: .bottom) {
+                ListView(viewModel: viewModel, selectedItem: $selectedItem)
+            }
+            .onTapGesture {
+                isShowingList = true
+            }
+            .onChange(of: selectedItem) { value in
+                name = value
+                viewModel.currencyChanged = true
+                
+               isShowingList = false
+                
+                
+            }
+            .onAppear{
+                viewModel.fetchSymbols()
+            }
             
             Spacer()
             
             Text(String(format: "%.2f", amount))
                 .minimumScaleFactor(0.5)
                 .padding(5)
+                //MARK: NEEDS FIX .blur(radius: name == selectedItem ? 2 : 0)
         }
         .frame(height: 35)
-        .background(.white.opacity(0.1))
+        
         .cornerRadius(5)
         .animation(.spring(), value: 3)
+    }
+}
+
+struct ListView: View{
+    @ObservedObject var viewModel: CurrencyViewModel
+    @Binding var selectedItem: String
+    
+    
+    var body: some View{
+        List(selection: $selectedItem) {
+            ForEach(0 ..< viewModel.allCurrencies.count, id: \.self) { index in
+                HStack {
+                    Text(viewModel.allCurrencies[index].name)
+                    Spacer()
+                    Text(viewModel.allCurrencies[index].symbol_native)
+                    
+                }
+                .font(.body).tag(viewModel.allCurrencies[index].code)
+                
+            }
+        }
+        .frame(height: 150)
     }
 }
 
